de1f8f7a38529568454eec884c364d4ebb3e6bd3
amei5 <amei5@illinois.edu>
2022-11-22 21:05:16 -0800

    added poshmark scraper, limited both scrapers to 3 results each

--- extension test/wiki/views.py
+++ extension test/wiki/views.py
@@ -7,8 +7,8 @@ import json
 from django.contrib.auth.models import User #####
 from django.http import JsonResponse , HttpResponse ####
 
-#import wikipedia
 import requests
+from requests import get
 import lxml
 from bs4 import BeautifulSoup
 import cchardet
@@ -19,20 +19,23 @@ def index(request):
 
 def get_ebay_summary(request):
     topic = request.GET.get('topic', None)
-    base_url = "https://www.ebay.com/sch/i.html?_nkw={}"
+    ebay_url = "https://www.ebay.com/sch/i.html?_nkw={}"
+    poshmark_url = "https://poshmark.com/search?query=jeans&type=listings&src=dir"
+
+    results = []
 
     print('topic:', topic)
 
-    url = base_url.format(topic)
+    #get ebay results
+
+    url = ebay_url.format(topic)
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'lxml')
-
-    results = []
 
     # skip first element since it's not an actual listing
     listings = soup.select('.s-item__wrapper.clearfix')[1:]
 
-    for item in listings:
+    for item in listings[:3]:
         try:
             title = item.select_one('.s-item__title').text
         except:
@@ -61,11 +64,40 @@ def get_ebay_summary(request):
         }
         results.append(result)
 
-    '''data = {
-        'summary': wikipedia.summary(topic, sentences=1),
-        'raw': 'Successful',
-    }'''
+    #get poshmark results
+    page = get(poshmark_url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    item_tiles = soup.find_all('div', class_='col-x12')
 
-    #print('json-data to be sent: ', data)
+    for tile in item_tiles[:3]:
+        try:
+            title = tile.find('a', class_='tile__title').get_text(strip=True)
+        except:
+            title = ''
+
+        try:
+            price_string = tile.find('span', class_="fw--bold").get_text(strip=True)
+            price = int(price_string.replace('$', ''))
+        except:
+            price = ''
+
+        try:
+            partial_link = tile.find('a', class_='tile__title').get('href')
+            link = 'http://www.poshmark.com' + partial_link
+        except:
+            link = ''
+
+        try:
+            image = tile.find('img').get('data-src')
+        except:
+            image = ''
+
+        result = {
+            'title': title,
+            'price': price,
+            'link': link,
+            'image': image,
+        }
+        results.append(result)
 
     return JsonResponse(results, safe=False)
