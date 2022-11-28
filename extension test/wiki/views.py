f0511475c24fdf854f24706e1ba527fdf508e236
adriennekb <acaby9@gmail.com>
2022-11-27 23:59:51 -0800

    test

--- extension test/wiki/views.py
+++ extension test/wiki/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-import json
 from django.contrib.auth.models import User #####
 from django.http import JsonResponse , HttpResponse ####
 
