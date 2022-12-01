51b6e209a4ec1660317ffb8f63937505ecdd1ab8
gyanggh <gyangweb@hotmail.com>
2022-11-30 21:18:24 -0800

    Update popup.js
    Update format

--- extension test/extension/popup.js
+++ extension test/extension/popup.js
@@ -13,8 +13,10 @@ $(function(){
 						//results_str = "";
 						results_str = "<p><table border='0'>";
 						for (let i = 0; i <results.length; i++) {
-						    results_str += "<p>"+(i+1) + ". " + "<a href=\"" + results[i].link + "\">" + results[i].title + "</a>" + " $" + results[i].price + "</p>" + "<img src=\"" + results[i].image + "\" alt=\"" + results[i].title + "\">";
+						    results_str += "<tr><td><p>"+(i+1) + ". " + "<a href=\"" + results[i].link + "\">" + results[i].title + "</a>" + " $" + results[i].price + "</p></td></tr><tr><td><img src=\"" + results[i].image + "\" alt=\"" + results[i].title + "\"></td></tr><tr><td> </td></tr>";
+						    //results_str += "<p>"+(i+1) + ". " + "<a href=\"" + results[i].link + "\">" + results[i].title + "</a>" + " $" + results[i].price + "</p>" + "<img src=\"" + results[i].image + "\" alt=\"" + results[i].title + "\">";
 						}
+						results_str += "</table></p>"
 						var htmlCode = "<html><body>" + results_str + "</body></html>";
 						var url = "data:text/html," + encodeURIComponent(htmlCode);
 
