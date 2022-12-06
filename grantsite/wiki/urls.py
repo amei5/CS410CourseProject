71beb4b2f40d7b983cd9fae5af1df48988858365
amei5 <amei5@illinois.edu>
2022-12-05 17:19:41 -0800

    cleaning up unused code

--- grantsite/wiki/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('get_wiki_summary/', views.get_wiki_summary, name='get_wiki_summary'),
-]
